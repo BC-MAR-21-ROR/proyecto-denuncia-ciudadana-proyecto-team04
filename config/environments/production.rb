1c1b228afefe2350fc979af83d05359ca401041d
Brayan Lopez <brayan.lopez@koombea.com>
2021-05-16 15:33:45 -0500

    add config for heroku and sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,15 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.default_url_options = { host: 'https://denunciaciudadana.herokuapp.com/' }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    domain: 'heroku.com',
+    user_name: 'apikey',
+    password: ENV['SENDGRID_API_KEY'],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+  config.action_mailer.delivery_method = :smtp
 end
